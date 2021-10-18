09617d864deacd8b164646d655a994f1723e1966
nesserinette <nesserine.touadi0@gmail.com>
2021-10-19 00:33:00 +0200

    activity 13 exo 3

--- src/main/java/dz/ibnrochd/master14/dao/PatientRepository.java
+++ src/main/java/dz/ibnrochd/master14/dao/PatientRepository.java
@@ -4,17 +4,19 @@ import java.util.List;
 
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 
 import dz.ibnrochd.master14.model.Patient;
 
 
-@Repository
+@RepositoryRestResource (collectionResourceRel="patients", path="patients")
 public interface PatientRepository extends JpaRepository<Patient, Integer> {
 
 	// TODO ajouter la signature d'une méthode pour rechercher des patients par leurs noms (convention Spring Data)
 	
-	     List <Patient> findByNom(String nom) ;
+	     List <Patient> findByNom(@Param("nom") String nom) ;
 	    
 	     
 	     List <Patient> findAll() ;
